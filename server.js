a5c42ce358897dd1b85c072290103d4da3ad7af8
Vikash Srivastava <VSrivastava@velocis.in>
2018-10-29 11:21:56 +0530

    two file commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/view/partial');
+
+app.set('view engine', 'hbs');
+
+//For Statis folder Access
+app.use(express.static(__dirname + '/html'));
+
+app.get('/about', (req, res)=>{
+
+     res.render('about.hbs', {
+     	PageTitle : "welcome vikash",
+     	Footer    : new Date().getFullYear()
+     });
+
+})
+
+//Test the Git
+app.get('/', (req, res)=>{
+	res.send(' First Web Node App for GIT Testing');
+	
+});
+
+
+app.listen(3000, ()=>{
+	console.log('Server started successsfully on port : 3000');
+});
+
+
